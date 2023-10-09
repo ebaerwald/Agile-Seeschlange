c7bb345c269d53fc621efab9f8a3a25acc61acd7
agr2104 <anna.girndt@stud.dhsh.de>
2023-10-09 10:54:16 +0200

    ScrollView RegisterPage

--- Frontend/pages/RegisterPage.jsx
+++ Frontend/pages/RegisterPage.jsx
@@ -1,93 +1,130 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native'; // Importieren Sie CheckBox von react-native
-import Button from '../components/Button';
-import DataInputField from '../components/DataInputField';
-import SnakeImage from '../components/SnakeImage';
-import Background from '../components/Background';
-import CustomCheckbox from '../components/Checkbox'; // Importieren Sie die CustomCheckbox-Komponente
-import HeaderText from '../components/HeaderText';
-import Text from '../components/Text';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native"; // Importieren Sie CheckBox von react-native
+import Button from "../components/Button";
+import DataInputField from "../components/DataInputField";
+import SnakeImage from "../components/SnakeImage";
+import Background from "../components/Background";
+import CustomCheckbox from "../components/Checkbox"; // Importieren Sie die CustomCheckbox-Komponente
+import HeaderText from "../components/HeaderText";
+import Text from "../components/Text";
+import { ScrollView } from "react-native";
 
 const RegisterPage = ({ navigation }) => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [birthdate, setBirthdate] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [agbChecked, setAgbChecked] = useState(false); // Zustand für die AGB-Checkbox
   const [datenschutzChecked, setDatenschutzChecked] = useState(false); // Zustand für die Datenschutz-Checkbox
   const [isChecked, setChecked] = useState(false);
 
   const handleLoginButtonClick = () => {
-    // Hier Logik für den Login-Button 
-    console.log('Login-Button wurde geklickt');
-    console.log('E-Mail:', email);
-    console.log('Passwort:', password);
+    // Hier Logik für den Login-Button
+    console.log("Login-Button wurde geklickt");
+    console.log("E-Mail:", email);
+    console.log("Passwort:", password);
   };
 
   const handleRegisterButtonClick = () => {
-    // Hier Logik für den Registrieren-Button 
-    console.log('Register-Button wurde geklickt');
-    console.log('Vorname:', firstName);
-    console.log('Nachname:', lastName);
-    console.log('Geburtsdatum:', birthdate);
-    console.log('E-Mail:', email);
-    console.log('Passwort:', password);
-    console.log('AGB akzeptiert:', agbChecked);
-    console.log('Datenschutzbestimmungen akzeptiert:', datenschutzChecked);
-    navigation.navigate('Register');
+    // Hier Logik für den Registrieren-Button
+    console.log("Register-Button wurde geklickt");
+    console.log("Vorname:", firstName);
+    console.log("Nachname:", lastName);
+    console.log("Geburtsdatum:", birthdate);
+    console.log("E-Mail:", email);
+    console.log("Passwort:", password);
+    console.log("AGB akzeptiert:", agbChecked);
+    console.log("Datenschutzbestimmungen akzeptiert:", datenschutzChecked);
+    navigation.navigate("Register");
   };
 
   return (
-    <Background showFooter={false} showBurgerBun={false}>
-      {/* Inhalt der Seite */}    
-      <View style={styles.outerBox}>
+    <ScrollView>
+      <Background showFooter={false} showBurgerBun={false}>
+        {/* Inhalt der Seite */}
+        <View style={styles.outerBox}>
+          <SnakeImage size="small" />
 
-        <SnakeImage size="small" />
+          <HeaderText title="Seeschlange" type="center" />
+          <Text
+            title="Gib deine Daten an, um die Weltmeere zu betreten und Weisheit zu finden, um anderen Seeschlangen zu helfen."
+            type="center"
+          />
 
-        <HeaderText title="Seeschlange" type="center" />
-        <Text title="Gib deine Daten an, um die Weltmeere zu betreten und Weisheit zu finden, um anderen Seeschlangen zu helfen." type="center" />
+          {/* Eingabefelder für Vorname, Nachname, Geburtsdatum, E-Mail und Passwort */}
+          <DataInputField
+            placeholder="Vorname*"
+            value={firstName}
+            onChangeText={(text) => setFirstName(text)}
+          />
+          <DataInputField
+            placeholder="Nachname*"
+            value={lastName}
+            onChangeText={(text) => setLastName(text)}
+          />
+          <DataInputField
+            placeholder="Geburtsdatum*"
+            value={birthdate}
+            onChangeText={(text) => setBirthdate(text)}
+          />
+          <DataInputField
+            placeholder="E-Mail*"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <DataInputField
+            placeholder="Passwort*"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
 
-        {/* Eingabefelder für Vorname, Nachname, Geburtsdatum, E-Mail und Passwort */}
-        <DataInputField placeholder="Vorname*" value={firstName} onChangeText={text => setFirstName(text)} />
-        <DataInputField placeholder="Nachname*" value={lastName} onChangeText={text => setLastName(text)} />
-        <DataInputField placeholder="Geburtsdatum*" value={birthdate} onChangeText={text => setBirthdate(text)} />
-        <DataInputField placeholder="E-Mail*" value={email} onChangeText={text => setEmail(text)} />
-        <DataInputField placeholder="Passwort*" value={password} onChangeText={text => setPassword(text)} />
+          {/* Checkboxen für AGB und Datenschutz, Verwenden Sie die CustomCheckbox-Komponente */}
+          <CustomCheckbox
+            label="Hiermit stimme ich den AGB zu*"
+            value={agbChecked}
+            onValueChange={(value) => setAgbChecked(value)}
+          />
+          <CustomCheckbox
+            label="Hiermit stimme ich den Datenschutzbestimmungen zu*"
+            value={datenschutzChecked}
+            onValueChange={(value) => setDatenschutzChecked(value)}
+          />
 
-        {/* Checkboxen für AGB und Datenschutz, Verwenden Sie die CustomCheckbox-Komponente */}
-        <CustomCheckbox label="Hiermit stimme ich den AGB zu*" value={agbChecked} onValueChange={value => setAgbChecked(value)} />
-        <CustomCheckbox label="Hiermit stimme ich den Datenschutzbestimmungen zu*" value={datenschutzChecked} onValueChange={value => setDatenschutzChecked(value)} />
+          <Text
+            title="Alle mit * markierten Felder sind Pflichtfelder. Bitte fülle sie aus."
+            type="center"
+          />
 
-        <Text title="Alle mit * markierten Felder sind Pflichtfelder. Bitte fülle sie aus." type="center" />
+          {/* Registrieren-Button */}
+          <Button
+            onPress={handleRegisterButtonClick}
+            iconType="Register"
+            text="Klicke hier um die Registrierung ab zuschließen!"
+          />
 
-      {/* Registrieren-Button */}
-      <Button
-        onPress={handleRegisterButtonClick}
-        iconType="Register"
-        text="Klicke hier um die Registrierung ab zuschließen!"
-      />
+          {/* Login-Button */}
+          <Button
+            onPress={handleLoginButtonClick}
+            iconType="Login"
+            text="Bereits eine Seeschlange? Dann logge dich hier ein"
+          />
 
-      {/* Login-Button */}
-      <Button
-        onPress={handleLoginButtonClick}
-        iconType="Login"
-        text="Bereits eine Seeschlange? Dann logge dich hier ein"
-      />
-
-      <Text title="Dieses Produkt wurde von ANG, TID, ERB, SMH entwickelt." type="center" />
-
-      </View>
-
-    </Background>
+          <Text
+            title="Dieses Produkt wurde von ANG, TID, ERB, SMH entwickelt."
+            type="center"
+          />
+        </View>
+      </Background>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   outerBox: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexDirection: "column",
+    alignItems: "center",
     padding: 10,
   },
 });
